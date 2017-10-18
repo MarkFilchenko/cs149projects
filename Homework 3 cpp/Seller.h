fe4007e5c12fa0783fe870527b97164a1ba0d127
avanibhatnagar <avanibhatnagar07@gmail.com>
2017-10-17 21:54:03 -0700

    Update Seller.h

--- Homework 3 cpp/Seller.h
+++ Homework 3 cpp/Seller.h
@@ -1,17 +1,4 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Seller.h
- * Author: Avani Bhatnagar
- *
- * Created on October 16, 2017, 5:38 PM
- */
-
-/*
  * Seller.h
  * CS 149 Group 9
  */
