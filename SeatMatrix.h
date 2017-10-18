691d73505ac02420b7b905f34d7d9656af52fff2
avanibhatnagar <avanibhatnagar07@gmail.com>
2017-10-17 21:29:11 -0700

    Add files via upload

---
+++ SeatMatrix.h
@@ -0,0 +1,30 @@
+/*
+ * SeatMatrix.h
+ * CS 149 Group 9
+ */
+
+#ifndef SEATMATRIX_H_
+#define SEATMATRIX_H_
+
+#include <string>
+#include <pthread.h>
+
+class SeatMatrix {
+private:
+	std::string seatGrid [10][10];
+	int highRow;//these variables make searching slightly faster
+	int medRow;
+	int lowRow;
+	pthread_mutex_t seatMutex;
+	int reserveHigh(int* row, int* col);//each type of customer needs different algorithms
+	int reserveMed(int* row, int* col);
+	int reserveLow(int* row, int* col);
+public:
+	SeatMatrix();
+	int reserveSeat(int* row, int* col, int priceType);
+	void giveSeat(int sellerNum, int priceType, int custNum, int row, int col);
+	std::string str();
+	virtual ~SeatMatrix();
+};
+
+#endif /* SEATMATRIX_H_ */
