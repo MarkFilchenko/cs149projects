e3a3316916e167dd23c5bc1586a32e2013feaf8a
avanibhatnagar <avanibhatnagar07@gmail.com>
2017-10-17 21:51:49 -0700

    Update PriceGuide.h

--- Homework 3 cpp/PriceGuide.h
+++ Homework 3 cpp/PriceGuide.h
@@ -1,15 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/* 
- * File:   PriceGuide.h
- * Author: Avani Bhatnagar
- *
- * Created on October 16, 2017, 5:37 PM
- */
 
 /*
  * PriceGuide.h
